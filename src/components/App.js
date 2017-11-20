fe7ab591ad1ced73812e2651e0bb63b231d3488e
David parker <adparker@fullsail.edu>
2017-11-19 21:25:23 -0500

    ; added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import Header from './Header'
-import Order from './Order'
-import Inventory from './Inventory'
-import simpleFishes from './sample-fishes'
-import Fish from './Fish'
-import base from '../base'
-import PropTypes from 'prop-types'
+import React from 'react';
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+import simpleFishes from './sample-fishes';
+import Fish from './Fish';
+import base from '../base';
+import PropTypes from 'prop-types';
 
 
 class App extends React.Component {
@@ -13,69 +13,69 @@ class App extends React.Component {
   state = {
     fishes: {},
     order: {},
-  }
+  };
 
   componentWillMount() {
     // This runs right before the <App> is rendered
     this.ref = base.syncState(`${this.props.params.storeId}/fishes`, {
       context: this,
       state: 'fishes',
-    })
+    });
 
     // Check if there is any order in localStorage
-    const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`)
+    const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
 
     if (localStorageRef) {
       // Update our App component's order state
       this.setState({
         order: JSON.parse(localStorageRef),
-      })
+      });
     }
 
   }
 
   componentWillUnmount() {
-    base.removeBinding(this.ref)
+    base.removeBinding(this.ref);
   }
 
   componentWillUpdate(nextProps, nextState) {
-    localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order))
+    localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order));
   }
-  addFish =(fish)=> {
+  addFish= (fish)=> {
     // Update the state
-    const copyFishes = {...this.state.fishes}
+    const copyFishes = {...this.state.fishes};
     //Add in new fish
-    const timestamp = Date.now()
-    copyFishes[`fish-${timestamp}`] = fish
+    const timestamp = Date.now();
+    copyFishes[`fish-${timestamp}`] = fish;
     //Set state
-    this.setState({fishes: copyFishes})
-  };
+    this.setState({fishes: copyFishes});
+  }
   updatefish = (key, updatedfish)=> {
-    const fishes = {...this.state.fishes}
-    fishes[key] = updatedfish
-    this.setState({fishes})
-  };
+    const fishes = {...this.state.fishes};
+    fishes[key] = updatedfish;
+    this.setState({fishes});
+  }
   deleteFish =(key) => {
-    const fishes = {...this.state.fishes}
+    const fishes = {...this.state.fishes};
     // Have to set to null because of the firebase database, otherwise use the delete keyword
-    fishes[key] = null
-    this.setState({fishes})
-  };
+    fishes[key] = null;
+    this.setState({fishes});
+  }
   deleteFromOrder = (key)=> {
-    const order = {...this.state.order}
-    delete order[key]
-    this.setState({order})
-  };
+    const order = {...this.state.order};
+    delete order[key];
+    this.setState({order});
+  }
   loadSamples = () => {
-    this.setState({fishes: simpleFishes})
-  };
+    this.setState({fishes: simpleFishes});
+  }
   addToOrder=(key)=> {
     // The ... make a copy of the state and puts it into the const
-    const copyOrder = {...this.state.order}
-    copyOrder[key] = copyOrder[key] + 1 || 1
-    console.log(key)
-    this.setState({order: copyOrder})
-  };
+    const copyOrder = {...this.state.order};
+    copyOrder[key] = copyOrder[key] + 1 || 1;
+    console.log(key);
+    this.setState({order: copyOrder});
+  }
   render() {
     return (
       // One big div around the app cuz react must keep to one parent element
@@ -111,10 +111,10 @@ class App extends React.Component {
           deleteFish={this.deleteFish}
         />
       </div>
-    )
+    );
   }
 }
 App.propTypes = {
   params: PropTypes.object.isRequired,
-}
-export default App
+};
+export default App;
